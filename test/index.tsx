aae84d49b8e456725bf14cd7dcd246686b130182
Luke Horvat <lukehorvat@gmail.com>
2024-02-24 23:23:29 +0100

    Define container component

--- test/index.tsx
+++ test/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Dock from '../dist';
+import { Dock, DockItem } from '../';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
@@ -10,17 +10,17 @@ root.render(
     width={Math.min(400, window.innerWidth * 0.5)}
     magnification={1}
     magnifyDirection="up"
-    debug={true}
+    debug
   >
     {['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'].map((letter) => (
-      <Dock.Item
+      <DockItem
         key={letter}
         width={0} // TODO: Should not need to specify this.
         className="dock-item"
         onClick={() => console.log(letter)}
       >
         <span>{letter}</span>
-      </Dock.Item>
+      </DockItem>
     ))}
   </Dock>
 );
