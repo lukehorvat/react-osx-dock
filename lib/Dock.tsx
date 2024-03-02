0a01040647d7c15ba59878d4860b701ecc651485
Luke Horvat <lukehorvat@gmail.com>
2024-03-02 21:23:53 +0100

    A few renames

--- lib/Dock.tsx
+++ lib/Dock.tsx
@@ -13,9 +13,42 @@ export function Dock(props: {
   children?: React.ReactNode;
 }) {
   const [magnifierX, setMagnifierX] = React.useState<number | null>(null);
+  const unmagnifiedItemWidth =
+    props.width / React.Children.count(props.children);
+  const unmagnifiedDockWidth =
+    unmagnifiedItemWidth * React.Children.count(props.children);
+  const magnifierRadius =
+    // The magnifier's "area of effect" in a particular direction.
+    unmagnifiedItemWidth * 3;
+  const maxOffsetWidth = magnifierRadius * props.magnification;
+  const maxDockWidth = unmagnifiedDockWidth + maxOffsetWidth;
+  const itemWidths = React.Children.map(props.children, (child, index) => {
+    if (magnifierX === null) return unmagnifiedItemWidth;
+    const itemCenter = unmagnifiedItemWidth * index + unmagnifiedItemWidth / 2;
+    const distanceFromItemCenter = Math.abs(magnifierX - itemCenter);
+    const magnifierStrength =
+      // An item is most magnified when the magnifier is positioned over its center.
+      Math.max(1 - distanceFromItemCenter / magnifierRadius, 0);
+    return unmagnifiedItemWidth * (1 + magnifierStrength * props.magnification);
+  })!;
+  const dockWidth = itemWidths.reduce((sum, itemWidth) => sum + itemWidth, 0);
+  const offsetWidth = Math.abs(maxDockWidth - dockWidth);
+  const leftOffsetWidth =
+    magnifierX === null
+      ? maxOffsetWidth / 2
+      : magnifierX < unmagnifiedDockWidth / 2
+      ? offsetWidth
+      : 0;
+  const rightOffsetWidth =
+    magnifierX === null
+      ? maxOffsetWidth / 2
+      : magnifierX >= unmagnifiedDockWidth / 2
+      ? offsetWidth
+      : 0;
+
   const onMagnify: React.MouseEventHandler = (event) => {
     const boundingBox = event.currentTarget.getBoundingClientRect();
-    const x = event.clientX - boundingBox.left - unmagnifiedDockOffset / 2;
+    const x = event.clientX - boundingBox.left - maxOffsetWidth / 2;
     if (x >= 0 && x < unmagnifiedDockWidth) {
       setMagnifierX(x);
     } else {
@@ -25,39 +58,6 @@ export function Dock(props: {
   const onUnmagnify = () => {
     setMagnifierX(null);
   };
-  const unmagnifiedDockItemWidth =
-    props.width / React.Children.count(props.children);
-  const magnifierRadius = unmagnifiedDockItemWidth * 3;
-  const unmagnifiedDockOffset = magnifierRadius * props.magnification;
-  const unmagnifiedDockWidth =
-    unmagnifiedDockItemWidth * React.Children.count(props.children);
-  const maxMagnifiedDockWidth = unmagnifiedDockWidth + unmagnifiedDockOffset;
-  const itemWidths = React.Children.map(props.children, (child, index) => {
-    if (magnifierX === null) return unmagnifiedDockItemWidth;
-    const itemCenter =
-      unmagnifiedDockItemWidth * index + unmagnifiedDockItemWidth / 2;
-    const distanceFromItemCenter = Math.abs(magnifierX - itemCenter);
-    const magnifierStrength =
-      // An item is most magnified when the magnifier is positioned over its center.
-      Math.max(1 - distanceFromItemCenter / magnifierRadius, 0);
-    return (
-      unmagnifiedDockItemWidth * (1 + magnifierStrength * props.magnification)
-    );
-  })!;
-  const dockWidth = itemWidths.reduce((sum, itemWidth) => sum + itemWidth, 0);
-  const dockOffset = Math.abs(maxMagnifiedDockWidth - dockWidth);
-  const offsetLeft =
-    magnifierX === null
-      ? unmagnifiedDockOffset / 2
-      : magnifierX < unmagnifiedDockWidth / 2
-      ? dockOffset
-      : 0;
-  const offsetRight =
-    magnifierX === null
-      ? unmagnifiedDockOffset / 2
-      : magnifierX >= unmagnifiedDockWidth / 2
-      ? dockOffset
-      : 0;
 
   return (
     <div
@@ -70,23 +70,23 @@ export function Dock(props: {
       }}
     >
       <DockOffset
-        width={offsetLeft}
-        height={unmagnifiedDockItemWidth}
+        width={leftOffsetWidth}
+        height={unmagnifiedItemWidth}
         magnifyDirection={props.magnifyDirection}
         debug={props.debug}
       />
       <DockItemsContainer
         backgroundClassName={props.backgroundClassName}
         itemWidths={itemWidths}
-        height={unmagnifiedDockItemWidth}
+        height={unmagnifiedItemWidth}
         magnifyDirection={props.magnifyDirection}
         debug={props.debug}
       >
         {props.children}
       </DockItemsContainer>
       <DockOffset
-        width={offsetRight}
-        height={unmagnifiedDockItemWidth}
+        width={rightOffsetWidth}
+        height={unmagnifiedItemWidth}
         magnifyDirection={props.magnifyDirection}
         debug={props.debug}
       />
